0f06f69d71f7cb708b7ca133133ac5ce6196fade
Adam Chader <64830256+Adchad@users.noreply.github.com>
2020-12-06 01:31:50 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,35 +2,251 @@
 var great_div_DOM = document.querySelector('.greatdiv');
 
 var xpadded,ypadded;
-var sizex,sizey;
+var tetro;
 
-
-sizex = 30;
-sizey = 30;
-
+var sizex = 8;
+var sizey = 20;
 
 var offsetx = 100;
 var offsety = 100;
 
-class Point{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class Obstacle{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-  }
-}
-
-var snake = [] ;
-
-var x,y;
-var dir = 'r';
 var speed = 0;
+var dir = 'd';
+
+
+class Point{
+
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.state=false;
+  }
+
+  getCoordinates(){
+    return [this.x,this.y];
+  }
+
+  setCoordinates(x,y){
+    this.x = x;
+    this.y = y;
+  }
+
+  getState(){
+    return this.state;
+  }
+  draw(){
+
+    if(this.state==true){
+      xpadded = ("00" + this.x).slice (-3);
+      ypadded = ("00" + this.y).slice (-3);
+    
+      document.querySelector("#indiv-"+ xpadded +"-"+ ypadded).style.backgroundColor = "black";
+    }
+    else{
+      xpadded = ("00" + this.x).slice (-3);
+      ypadded = ("00" + this.y).slice (-3);
+    
+      document.querySelector("#indiv-"+ xpadded +"-"+ ypadded).style.backgroundColor = "white";
+    }
+  }
+
+
+  add(){
+    this.state=true;
+  }
+  remove(){
+    this.state=false;
+  }
+
+
+
+
+}
+
+var shapes =[
+  [
+  
+    [ 1 , 1 ],
+    [ 1 , 1 ]
+  ],
+
+  [
+
+    [ 1 , 0 , 0 ],
+    [ 1 , 0 , 0 ],
+    [ 1 , 1 , 0 ]
+  ],
+
+  [
+    [ 0 , 0 , 1 ],
+    [ 0 , 0 , 1 ],
+    [ 0 , 1 , 1 ]
+  ],
+
+  [
+    [ 1 , 0 , 0 , 0],
+    [ 1 , 0 , 0 , 0],
+    [ 1 , 0 , 0 , 0],
+    [ 1 , 0 , 0 , 0]
+  ],
+
+  [
+
+    [ 1 , 0 , 0],
+    [ 1 , 1 , 0],
+    [ 1 , 0 , 0]
+  ]
+
+]
+
+
+function generateShape(){
+  return shapes[Math.floor(Math.random() * shapes.length)];
+}
+
+class Tetromino {
+  constructor(map,shape, x, y){
+
+    this.map=map;
+    this.x=x;
+    this.y=y;
+    this.shape = JSON.parse(JSON.stringify(shape)); 
+    this.array=[];
+    this.shape_size=this.shape.length;
+
+  }
+
+  update() {
+    this.array=[];
+    for(var i=0; i<this.shape_size; ++i){
+      for(var j=0; j<this.shape_size; ++j){
+        if(this.shape[i][j]===1){
+          this.array.push(this.map[this.y+j][this.x+i])	;
+        }
+      }
+    }
+  }
+
+  rotate(){
+    var old_shape=JSON.parse(JSON.stringify(this.shape));
+    var old_array=JSON.parse(JSON.stringify(this.array));
+
+
+    for( var i = 0 ; i<this.shape_size/2 ; i++ ){
+      for(var j = i; j< this.shape_size- i -1; j++ ) {
+        var temp = this.shape[i][j];
+        this.shape[i][j] = this.shape[j][this.shape_size - 1 -i];
+        this.shape[ j][this.shape_size - 1 -i] = this.shape[this.shape_size - 1 - i][this.shape_size - 1 - j];
+        this.shape[this.shape_size - 1 - i][this.shape_size - 1 - j] = this.shape[this.shape_size - 1 -j][ i];
+        this.shape[this.shape_size - 1 -j][ i] = temp;
+      }
+    }
+    this.update();
+
+    for(var i of this.array){
+      if(old_array.includes(i)===false){
+        var coord = i.getCoordinates();
+        var cell=this.map[coord[1]][coord[0]];
+        if(cell.getState()==true){
+
+          console
+          this.shape=JSON.parse(JSON.stringify(old_shape));
+          this.update();
+          return
+        }
+      }
+     
+    }
+
+
+
+  }
+
+  move(direction){
+    if(this.detectCollision(direction)===false){
+      switch (direction) {
+        case 'd':
+          this.y++;
+          break;
+    
+        case 'l':
+          this.x--;
+          break;
+        case 'r':
+          this.x++;
+          break;
+      }
+      this.update();
+    }
+  }
+
+  draw(){
+    for(var i=0; i<this.array.length; ++i){
+      this.array[i].add();
+    }
+  }
+
+  erase(){
+    for(var i=0; i<this.array.length; ++i){
+      this.array[i].remove();
+    }
+  }
+
+  rotateAndDraw(){
+    this.erase();
+    this.rotate();
+    this.draw();
+  }
+
+  moveAndDraw(direction){
+    this.erase();
+    this.move(direction);
+    this.draw();
+  }
+
+  print(){
+    
+      console.log(this.y);
+    
+  }
+
+  detectCollision(direction){
+    
+    for(var i of this.array){
+      var coord = i.getCoordinates();
+
+      switch (direction) {
+        case 'd':
+          var next_cell=this.map[coord[1]+1][coord[0]];
+          break;
+        case 'l':
+          var next_cell=this.map[coord[1]][coord[0]-1];
+          break;
+        case 'r':
+          var next_cell=this.map[coord[1]][coord[0]+1];
+          break;
+      }
+  
+      if(this.array.includes(next_cell)==false){
+        if(next_cell.getState()===true){
+          return true;
+        }
+      }
+
+    }
+    return false;
+  }
+
+
+
+
+
+}
+
+
+
+
+
 
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -53,61 +269,12 @@ function init(){
 }
 
 
-//.shift() enlève le debut de la liste
-//.push() append at end
-
-function draw_point(x,y){
-
-  snake.push(new Point(x,y));
-
-  xpadded = ("00" + x).slice (-3);
-  ypadded = ("00" + y).slice (-3);
-
-  document.querySelector("#indiv-"+ xpadded +"-"+ ypadded).style.backgroundColor = "black";
-
-  var a = snake.shift();
-
-  xpadded = ("00" + a.x).slice (-3);
-  ypadded = ("00" + a.y).slice (-3);
-
-  document.querySelector("#indiv-"+ xpadded +"-"+ ypadded).style.backgroundColor = "white";
-
-
+function rotate_current_Tetro(){
+  tetro.rotateAndDraw();
 }
-
-function draw_obs(x,y){
-
-
-  xpadded = ("00" + x).slice (-3);
-  ypadded = ("00" + y).slice (-3);
-
-  document.querySelector("#indiv-"+ xpadded +"-"+ ypadded).style.backgroundColor = "red";
-
-}
-
-function erase_pnt(x,y){
-
-
-  xpadded = ("00" + x).slice (-3);
-  ypadded = ("00" + y).slice (-3);
-
-  document.querySelector("#indiv-"+ xpadded +"-"+ ypadded).style.backgroundColor = "black";
-
-}
-
-
-
-
-document.addEventListener('keydown', logKey);
 
 function logKey(e) {
   switch (e.code) {
-    case 'ArrowUp':
-      dir = 'u'
-      break;
-    case 'ArrowDown':
-      dir= 'd';
-      break;
 
     case 'ArrowLeft':
       dir = 'l';
@@ -117,68 +284,153 @@ function logKey(e) {
       dir = 'r';
       break;
 
-
+    case 'Space':
+      rotate_current_Tetro();
+      break;
   }
 }
 
 
 
+class Map {
+
+  constructor(width,height){
+
+    this.width=width;
+    this.height=height;
+    this.map=[];
+
+    for(var i=0; i<this.height; ++i){
+      this.map.push(new Array());
+      for(var j=0; j<this.width; ++j){
+        this.map[i].push(new Point(j,i));
+
+        if(i==0 || i==this.height-1 ||j==0||j==this.width-1){
+          this.map[i][j].add();
+        }
+
+      }
+    }
 
 
 
+  }
 
-x=0;
-y=0;
-for (var i = 0; i < 5; i++) {
-    snake.push(new Point(x,y))
-    x++;
+  draw(){
+    for(var i=0; i<this.height; ++i){
+      this.map.push(new Array());
+      for(var j=0; j<this.width; ++j){
+        this.map[i][j].draw();
+      }
+    }
+  }
+
+  getMap(){
+    return this.map;
+  }
+
+  testLine(){
+    var ctr=0;
+    for(var i=1; i<this.height-1;i++){
+      ctr=0;
+      for(var j=0; j<this.width; ++j){
+        if(this.map[i][j].getState()===true){
+          ctr++;
+        }
+      }
+
+      if(ctr===this.width){
+        return i;
+      }
+
+    }
+
+    return 0;
+    //     console.log('LIGNE');
+
+    //     for(var i2=i; i2>1 ; i2--){
+    //       for(var j2=0; j2<this.width;j2++){
+    //         this.map[i2][j2]=this.map[i2-1][j2];
+    //         this.map[i2][j2].setCoordinates(j2,i2-1);
+    //       }
+    //     }
+
+    //     for(var j3=0; j3<this.width;j3++){
+    //       this.map[1][j3]=new Point(j3,1);
+    //     }
+
+    //   }
+    // }
+  }
+
+
+
+  clearLine(line){
+    for(var i=line; i>1 ; i--){
+      for(var j=0; j<this.width;j++){
+        this.map[i][j]=this.map[i-1][j];
+        this.map[i][j].setCoordinates(j,i);
+      }
+    }
+
+    for(var j=0; j<this.width;j++){
+      this.map[1][j]=new Point(j,1);
+      if(j==0 || j==this.width-1){
+        this.map[1][j].add();
+      }
+    }
+
+  }
 }
+  
 
-var obs = new Obstacle(5,5 );
 
+
+
+
+var map=new Map(sizex,sizey);
+
+
+
+
+
+
+
+//MAIN
+
+document.addEventListener('keydown', logKey);
 init();
 
+tetro = new Tetromino(map.getMap(),generateShape(),2,0);
+//GAME LOOP
 async function loop(timestamp) {
+
   await sleep(400-speed);
 
-  draw_obs(obs.x, obs.y);
+  tetro.moveAndDraw(dir);
 
-  if(x == obs.x && y == obs.y){
-    snake.unshift(new Point (0,0));
+  if(tetro.detectCollision('d')){
+    var line=map.testLine();
+    if(line!=0){
+      map.clearLine(line);    
+    }
+    tetro = new Tetromino(map.getMap(),generateShape(),2,0);
+  };
 
-    speed += 30;
-    erase_pnt(obs.x, obs.y);
-    obs = new Obstacle(Math.floor(Math.random() * sizex) , Math.floor(Math.random() * sizey));
+  
+  
+  //tetro.print();
+  //console.log(dir); 
+  
+  dir='d';
 
-
-  }
-
-  switch (dir) {
-    case 'u':
-      y--;
-      draw_point(x,y);
-      break;
-    case 'd':
-      y++;
-      draw_point(x,y);
-      break;
-
-    case 'l':
-      x--;
-      draw_point(x,y);
-      break;
-
-    case 'r':
-      x++;
-      draw_point(x,y);
-      break;
+  map.draw();
 
 
+  
 
-  }
-
+  //console.table(map);
+  
   window.requestAnimationFrame(loop)
-}
-
-
+} 
 window.requestAnimationFrame(loop)
